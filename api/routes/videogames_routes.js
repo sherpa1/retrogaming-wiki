a9666556ee5065717f66fab514759131eb4f7e86
Alexandre Leroux <alex@sherpa.one>
2020-12-03 14:44:46 +0100

    hateoas videogames/id

--- api/routes/videogames_routes.js
+++ api/routes/videogames_routes.js
@@ -26,4 +26,32 @@ router.get('/', async (req, res, next) => {
     );
 });
 
+router.get('/:id', async (req, res, next) => {
+
+    if (!req.params.id || req.params.id == undefined || req.params.id == 0) return res.status(401).location(req.path).json({ message: "Missing videogame id" });
+
+    const the_videogame = videogames.find((videogame) =>
+        (videogame.id === Number(req.params.id)));
+
+    if(the_videogame==undefined)
+    return res.status(404).location(req.path).json({ message: "Not Found" });
+
+    res.status(200).location(req.path).json(
+        {
+            data:the_videogame,
+            type:"videogames",
+            links:[
+                {
+                    rel:"self", 
+                    href:`${base_url}/${req.params.id}`
+                },
+                {
+                    rel:"list", 
+                    href:`${base_url}`
+                }
+            ],
+        }
+    );
+});
+
 module.exports = router;
