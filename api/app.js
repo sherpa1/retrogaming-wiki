7342a25110386262ac765598d0d2b3a52339a147
Alexandre Leroux <alex@sherpa.one>
2020-12-03 14:00:41 +0100

    refactoring env

--- api/app.js
+++ api/app.js
@@ -3,8 +3,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.LOCAL_PORT || 3000;
-const host = process.env.HOST || "http://localhost";
+const {local_port,host} = require("./config/env");
 
 const videogames_routes = require("./routes/videogames_routes");
 app.use("/videogames",videogames_routes);
@@ -13,6 +12,6 @@ app.get('/', async (req, res, next) => {
     res.status(200).location(req.path).json({ status: 200, message:"Welcome to the Retrogaming API" });
 });
 
-app.listen(port, async () => {
-    console.log(`Retrograming Wiki API listening at ${host}:${port}`);
+app.listen(local_port, async () => {
+    console.log(`Retrograming Wiki API listening at ${host}:${local_port}`);
 });
--- api/routes/videogames_routes.js
+++ api/routes/videogames_routes.js
@@ -2,9 +2,12 @@
 const express = require('express');
 const router = express.Router();
 
+const {dist_port, local_port,host} = require("../config/env");
+
 const videogames = require("../data/videogames");//Video Games Static Data
 
-const base_url = `${process.env.HOST}:${process.env.DIST_PORT}/videogames`; 
+
+const base_url = `${host}:${dist_port}/videogames`; 
 
 router.get('/', async (req, res, next) => {
     res.status(200).location(req.path).json(videogames);
